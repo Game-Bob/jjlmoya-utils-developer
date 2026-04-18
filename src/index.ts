91d0e27c6368fe1095b61b19eb05d3a0b75eed49
jjlmoya <juanjo1989@gmail.com>
2026-04-18 21:28:52 +0200

    fix: remove re-exports of removed component vars from main index

--- src/index.ts
+++ src/index.ts
@@ -17,74 +17,50 @@ export type {
 
 export { ALL_ENTRIES, ALL_TOOLS } from './tools';
 
-export { JsonFormatterComponent, JsonFormatterSEO, JsonFormatterBibliography } from './tool/jsonFormatter/index';
 export { JSON_FORMATTER_TOOL } from './tool/jsonFormatter/index';
 
-export { SvgToCssComponent, SvgToCssSEO, SvgToCssBibliography } from './tool/svgToCss/index';
 export { SVG_TO_CSS_TOOL } from './tool/svgToCss/index';
 
-export { AspectRatioComponent, AspectRatioSEO, AspectRatioBibliography } from './tool/aspectRatio/index';
 export { ASPECT_RATIO_TOOL } from './tool/aspectRatio/index';
 
-export { PlaceholderGeneratorComponent, PlaceholderGeneratorSEO, PlaceholderGeneratorBibliography } from './tool/placeholderGenerator/index';
 export { PLACEHOLDER_GENERATOR_TOOL } from './tool/placeholderGenerator/index';
 
-export { UrlEncoderDecoderComponent, UrlEncoderDecoderSEO, UrlEncoderDecoderBibliography } from './tool/urlEncoderDecoder/index';
 export { URL_ENCODER_DECODER_TOOL } from './tool/urlEncoderDecoder/index';
 
-export { DuplicateCssRemoverComponent, DuplicateCssRemoverSEO, DuplicateCssRemoverBibliography } from './tool/duplicateCssRemover/index';
 export { DUPLICATE_CSS_REMOVER_TOOL } from './tool/duplicateCssRemover/index';
 
-export { CssToInlineConverterComponent, CssToInlineConverterSEO, CssToInlineConverterBibliography } from './tool/cssToInlineConverter/index';
 export { CSS_TO_INLINE_CONVERTER_TOOL } from './tool/cssToInlineConverter/index';
 
-export { CssSpecificityCalculatorComponent, CssSpecificityCalculatorSEO, CssSpecificityCalculatorBibliography } from './tool/cssSpecificityCalculator/index';
 export { CSS_SPECIFICITY_CALCULATOR_TOOL } from './tool/cssSpecificityCalculator/index';
 
-export { CronGeneratorComponent, CronGeneratorSEO, CronGeneratorBibliography } from './tool/cronGenerator/index';
 export { CRON_GENERATOR_TOOL } from './tool/cronGenerator/index';
 
-export { KeycodeComponent, KeycodeSEO, KeycodeBibliography } from './tool/keycode/index';
 export { KEYCODE_TOOL } from './tool/keycode/index';
 
-export { LlmCostCalculatorComponent, LlmCostCalculatorSEO, LlmCostCalculatorBibliography } from './tool/llmCostCalculator/index';
 export { LLM_COST_CALCULATOR_TOOL } from './tool/llmCostCalculator/index';
 
-export { MusicalTypographyComponent, MusicalTypographySEO, MusicalTypographyBibliography } from './tool/musicalTypography/index';
 export { MUSICAL_TYPOGRAPHY_TOOL } from './tool/musicalTypography/index';
 
-export { MobileMockupGeneratorComponent, MobileMockupGeneratorSEO, MobileMockupGeneratorBibliography } from './tool/mobileMockupGenerator/index';
 export { MOBILE_MOCKUP_GENERATOR_TOOL } from './tool/mobileMockupGenerator/index';
 
-export { HashGeneratorComponent, HashGeneratorSEO, HashGeneratorBibliography } from './tool/hashGenerator/index';
 export { HASH_GENERATOR_TOOL } from './tool/hashGenerator/index';
 
-export { PromptLibraryComponent, PromptLibrarySEO, PromptLibraryBibliography } from './tool/promptLibrary/index';
 export { PROMPT_LIBRARY_TOOL } from './tool/promptLibrary/index';
 
-export { ColorConverterComponent, ColorConverterSEO, ColorConverterBibliography } from './tool/colorConverter/index';
 export { COLOR_CONVERTER_TOOL } from './tool/colorConverter/index';
 
-export { ReadabilityCalculatorComponent, ReadabilityCalculatorSEO, ReadabilityCalculatorBibliography } from './tool/readabilityCalculator/index';
 export { READABILITY_CALCULATOR_TOOL } from './tool/readabilityCalculator/index';
 
-export { SvgSanitizerComponent, SvgSanitizerSEO, SvgSanitizerBibliography } from './tool/svgSanitizer/index';
 export { SVG_SANITIZER_TOOL } from './tool/svgSanitizer/index';
 
-export { UtmGeneratorComponent, UtmGeneratorSEO, UtmGeneratorBibliography } from './tool/utmGenerator/index';
 export { UTM_GENERATOR_TOOL } from './tool/utmGenerator/index';
 
-export { UrlCleanerComponent, UrlCleanerSEO, UrlCleanerBibliography } from './tool/urlCleaner/index';
 export { URL_CLEANER_TOOL } from './tool/urlCleaner/index';
 
-export { GeneradorSecurityTxtComponent, GeneradorSecurityTxtSEO, GeneradorSecurityTxtBibliography } from './tool/generadorSecurityTxt/index';
 export { GENERADOR_SECURITY_TXT_TOOL } from './tool/generadorSecurityTxt/index';
 
-export { CalculadoraTiempoDatosComponent, CalculadoraTiempoDatosSEO, CalculadoraTiempoDatosBibliography } from './tool/calculadoraTiempoDatos/index';
 export { CALCULADORA_TIEMPO_DATOS_TOOL } from './tool/calculadoraTiempoDatos/index';
 
-export { InspectorCertificadosSslComponent, InspectorCertificadosSslSEO, InspectorCertificadosSslBibliography } from './tool/inspectorCertificadosSsl/index';
 export { INSPECTOR_CERTIFICADOS_SSL_TOOL } from './tool/inspectorCertificadosSsl/index';
 
-export { ConversorExcelCsvHtmlComponent, ConversorExcelCsvHtmlSEO, ConversorExcelCsvHtmlBibliography } from './tool/conversorExcelCsvHtml/index';
 export { CONVERSOR_EXCEL_CSV_HTML_TOOL } from './tool/conversorExcelCsvHtml/index';
